f7c3536764a034d4491c825ff163a9217998136c
raiayush2610 <ayushayushravi@gmail.com>
2024-02-19 13:31:42 +0530

    npm install

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const cors = require('cors');
 const app = express();
 app.use(cors({origin:'*'}));
 mongoose.set('strictQuery', false);
+
 app.use(express.json());
-app.use(cors({origin: '*'}))
 
 // Mogodb Collection
 mongoose.connect(process.env.DB)
